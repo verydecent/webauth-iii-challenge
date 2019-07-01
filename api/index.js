f00ca952762b5335e4f9afd30d7a6676005585f9
verydecent <wnjhwng@gmail.com>
2019-07-01 13:18:59 -0700

    Imports usersRoutes.js

--- api/index.js
+++ api/index.js
@@ -1,12 +1,13 @@
 const express = require('express');
 
 const authRouter = require('../auth/auth-router');
-
+const userRouter = require('../users/users-router');
 const server = express();
 
 server.use(express.json());
 
 server.use('/api/auth', authRouter);
+server.use('/api/users', userRouter);
 
 // Sanity check
 server.get('/', (req, res) => {
