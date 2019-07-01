b601abaf443918081e1f78ee1b640d9a09519d01
verydecent <wnjhwng@gmail.com>
2019-07-01 13:19:48 -0700

    Cannot use single variable reference point to reassign to location.....

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -4,14 +4,15 @@ const bcrypt = require('bcryptjs');
 const Database = require('../users/users-model');
 
 router.post('/register', (req, res) => {
-  let { username, password } = req.body;
+  let user = req.body;
+  let { username, password } = user;
 
   if (!username || !password) {
     res.status(400).json({ error: "You are missing either or both of the following fields: username, password" });
   }
   else {
     let hashedPassword = bcrypt.hashSync(password, 14);
-    password = hashedPassword;
+    user.password = hashedPassword;
 
     Database.add(req.body)
       .then(id => {
@@ -23,8 +24,24 @@ router.post('/register', (req, res) => {
   }
 });
 
-// router.post('/login', (req, res) => {
+router.post('/login', (req, res) => {
+  let { username, password } = req.body;
 
-// });
+  Database.findBy(username)
+    .then(user => {
+      console.log("req pw - " , password);
+      console.log("db pw - ", user.password);
+      console.log(bcrypt.compareSync(password, user.password));
+      if (bcrypt.compareSync(password, user.password)) {
+        res.status(200).json({ user, password })
+      }
+      else {
+        res.status(400).json({ error: "Wrong password" });
+      }
+    })
+    .catch(err =>{
+      res.status(500).json({ error: "Error logging in" });
+    });
+});
 
 module.exports = router;
