eb6afb4dcc081a7889c7e31a9967a04e794d43f9
verydecent <wnjhwng@gmail.com>
2019-07-01 12:18:58 -0700

    Import authRouter

--- api/index.js
+++ api/index.js
@@ -1,8 +1,12 @@
 const express = require('express');
 
+const authRouter = require('../auth/auth-router');
+
 const server = express();
 
 server.use(express.json());
+
+server.use('/api/auth', authRouter);
 
 // Sanity check
 server.get('/', (req, res) => {
