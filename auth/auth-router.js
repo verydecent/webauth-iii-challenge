417532a32e1017e4c842ab58471358a0f599739a
verydecent <wnjhwng@gmail.com>
2019-07-01 12:15:58 -0700

    1st attempt at auth/register

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,3 +1,29 @@
 const router = require('express').Router();
+const bcrypt = require('bcryptjs');
+
+router.post('/register', (req, res) => {
+  const { username, password } = req.body;
+
+  if (!username || !password) {
+    res.status(400).json({ error: "You are missing either or both of the following fields: username, password" });
+  }
+  else {
+    const hashedPassword = bcrypt.hashSync(password, 14);
+    password = hashedPassword;
+    console.log(password);
+
+    Database.add(req.body)
+      .then(id => {
+        res.status(200).json({ success: `User ${id} was added` });
+      })
+      .catch(err => {
+        res.status(500).json({ error: "Server error" });
+      });
+  }
+});
+
+router.post('/login', (req, res) => {
+
+});
 
 module.exports = router;
